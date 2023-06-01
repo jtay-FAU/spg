70f629ed86f4f6374c664eb617be75e5f4e4b58c
jtay-FAU <57227522+jtay-FAU@users.noreply.github.com>
2023-06-01 13:15:24 +0100

    spg_app.py

--- spg_app.py
+++ spg_app.py
@@ -29,80 +29,90 @@ def check_password():
         return True
 
 if check_password():
-    import requests
-    from bs4 import BeautifulSoup
-    import openai
-    from PIL import Image
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import openai
+from PIL import Image
 
-    # Access OpenAI API key from Streamlit Secrets
-    openai.api_key = st.secrets["openai"]["api_key"]
+# Access OpenAI API key from Streamlit Secrets
+openai.api_key = st.secrets["openai"]["api_key"]
 
-    def fetch_text_from_url(url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        h1_title = soup.find('h1').get_text().strip() if soup.find('h1') else ""
-        bespoke_page = soup.find(id='bespokePage', class_='bespokePage')
+@st.cache
+def fetch_text_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    h1_title = soup.find('h1').get_text().strip() if soup.find('h1') else ""
+    bespoke_page = soup.find(id='bespokePage', class_='bespokePage')
 
-        if bespoke_page:
-            for table in bespoke_page.find_all(class_='table'):
-                table.extract()
-            for info_box in bespoke_page.find_all(class_='infoBox'):
-                info_box.extract()
+    if bespoke_page:
+        for table in bespoke_page.find_all(class_='table'):
+            table.extract()
+        for info_box in bespoke_page.find_all(class_='infoBox'):
+            info_box.extract()
+        for card_body in bespoke_page.find_all(class_='card-body'):
+            card_body.extract()
+        for territory in bespoke_page.find_all(class_='territory'):
+            territory.extract()
 
-            text = bespoke_page.get_text().strip()
-        else:
-            text = "No text found in the specified div."
+        text = bespoke_page.get_text().strip()
+    else:
+        text = "No text found in the specified div."
 
-        return h1_title, text
+    return h1_title, text
 
-    def generate_social_media_posts(text):
-        social_media_platforms = {
-            "Twitter": "twitter_logo.png",
-            "LinkedIn": "linkedin_logo.png",
-            "Facebook": "facebook_logo.png",
-            "TikTok": "tiktok_logo.png",
-            "Instagram": "instagram_logo.png"
-        }
-        posts = {}
+def generate_social_media_posts(text, platforms):
+    social_media_platforms = {
+        "Twitter": "twitter_logo.png",
+        "LinkedIn": "linkedin_logo.png",
+        "Facebook": "facebook_logo.png",
+        "TikTok": "tiktok_logo.png",
+        "Instagram": "instagram_logo.png"
+    }
+    posts = {}
 
-        for platform, logo_file in social_media_platforms.items():
-            prompt = f"Create a social media post to promote the following article on {platform}. The post should be engaging and tailored to the {platform} audience. Text: \n\n{text}\n\n{platform} Post:"
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=prompt,
-                max_tokens=1000,
-                n=1,
-                stop=None,
-                temperature=0.7,
-                top_p=1
-            )
-            post = response.choices[0].text.strip()
-            posts[platform] = (post, logo_file)
+    for platform in platforms:
+        logo_file = social_media_platforms[platform]
+        prompt = f"Create a social media post to promote the following article on {platform}. The post should be engaging and tailored to the {platform} audience. Text: \n\n{text}\n\n{platform} Post:"
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1000,
+            n=1,
+            stop=None,
+            temperature=0.7,
+            top_p=1
+        )
+        post = response.choices[0].text.strip()
+        posts[platform] = (post, logo_file)
 
-        return posts
+    return posts
 
-    def main():
-        st.title("Social Media Post Generator")
+def main():
+    st.title("Social Media Post Generator")
 
-        url = st.text_input("Enter the URL:")
-        if st.button("Generate Posts"):
-            h1_title, text = fetch_text_from_url(url)
+    url = st.text_input("Enter the URL:")
+    platforms = st.multiselect("Select social media platforms:", ["Twitter", "LinkedIn", "Facebook", "TikTok", "Instagram"])
+    
+    if st.button("Generate Posts"):
+        h1_title, text = fetch_text_from_url(url)
 
-            st.subheader("H1 Title:")
-            st.text(h1_title)
+        st.subheader("H1 Title:")
+        st.text(h1_title)
 
-            st.subheader("URL:")
-            st.text(url)
+        st.subheader("URL:")
+        st.text(url)
 
-            st.subheader("Extracted Text:")
-            st.text(text)
+        st.subheader("Extracted Text:")
+        st.text(text)
 
-            st.subheader("Generated Social Media Posts:")
-            posts = generate_social_media_posts(text)
-            for platform, (post, logo_file) in posts.items():
-                st.subheader(platform)
-                st.image(Image.open(logo_file), use_column_width=True)
-                st.text(post)
+        st.subheader("Generated Social Media Posts:")
+        posts = generate_social_media_posts(text, platforms)
+        for platform, (post, logo_file) in posts.items():
+            st.subheader(platform)
+            logo_image = Image.open(logo_file).resize((32, 32))
+            st.image(logo_image, use_column_width=True)
+            st.text(post)
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
